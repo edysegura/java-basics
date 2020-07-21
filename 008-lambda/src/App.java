de6d6ea8581fd1ed6400d9cd5a4b503b0a6f9800
Edy Segura <edysegura@gmail.com>
2020-07-21 15:31:18 -0300

    Change indentation to use 2 spaces

--- 008-lambda/src/App.java
+++ 008-lambda/src/App.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("Davi");
-        names.add("Lidy");
-        names.add("Edy");
+  public static void main(String[] args) {
+    List<String> names = new ArrayList<>();
+    names.add("Davi");
+    names.add("Lidy");
+    names.add("Edy");
 
-        names.forEach(name -> System.out.println(name));
-    }
+    names.forEach(name -> System.out.println(name));
+  }
 }
