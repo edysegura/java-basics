b16a13a746885c2b7705993431762d59c266e44f
Edy Segura <edysegura@gmail.com>
2020-07-27 10:48:10 -0300

    Refactor lambda example

--- 008-lambda/src/App.java
+++ 008-lambda/src/App.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class App {
   public static void main(String[] args) {
-    List<String> names = new ArrayList<>();
-    names.add("Davi");
-    names.add("Lidy");
-    names.add("Edy");
+    List<String> names = Arrays.asList("Davi", "Lidy", "Edy");
 
     names
       .stream()
